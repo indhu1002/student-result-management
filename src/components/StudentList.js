24122143d7d77232c61180d3c4ae8d82b657ae95
Indhu <indhudande10.com>
2025-06-25 08:03:08 +0530

    updated code

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,39 +1,3 @@
-// // src/components/StudentList.js
-// import React, { useEffect, useState } from "react";
-// import { db } from "../firebase";
-// import { ref, onValue, remove } from "firebase/database";
-
-// function StudentList() {
-//   const [students, setStudents] = useState({});
-
-//   useEffect(() => {
-//     const dbRef = ref(db, "students/");
-//     onValue(dbRef, (snapshot) => {
-//       setStudents(snapshot.val() || {});
-//     });
-//   }, []);
-
-//   const deleteStudent = (roll) => {
-//     remove(ref(db, "students/" + roll));
-//   };
-
-//   return (
-//     <div>
-//       <h3>Student Records</h3>
-//       {Object.entries(students).map(([roll, stu]) => (
-//         <div key={roll}>
-//           <p>Roll: {roll} | Name: {stu.name} | Subject: {stu.subject} | Marks: {stu.marks}</p>
-//           <button onClick={() => deleteStudent(roll)}>Delete</button>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default StudentList;
-
-
-// src/components/StudentList.js
 import React, { useEffect, useState } from "react";
 import "./StudentList.css";
 import { db } from "../firebase";
